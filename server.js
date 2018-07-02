d3ad8647fa68e379330bd9dd7ba9c5f5720e94f4
xubayr007 <xubayr007@gmail.com>
2018-07-02 21:53:18 +0530

    mongolab

--- server.js
+++ server.js
@@ -2,19 +2,39 @@ const express = require('express')
 const app = express()
 const router = express.Router()
 const morgan = require('morgan')
+var mongoose = require('mongoose')
+var User = require('./models/user')
+const bodyParser = require('body-parser')
 
+mongoose.connect('mongodb://root:testing123@ds125341.mlab.com:25341/ecommerce', function (err) {
 
+    if (err)
+        console.log(err)
+    else
+        console.log('connected')
+})
 
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
 app.use(morgan('dev'))
 
-app.get('/home',(req, res)=>{
+app.post('/user', (req, res,next) => {
+    var user = new User()
+    user.profile.name = req.body.name
+    user.password = req.body.password
+    user.email = req.body.email
 
-res.send('hi i am hollowen')
+    user.save(function(err){
+        if(err)
+        next(err)
+
+        res.send('Successfully created new user')
+    })
 
 })
 
 
-app.listen(3000,()=>{
+app.listen(3000, () => {
     console.log('server listening on port 3000')
 })
 
